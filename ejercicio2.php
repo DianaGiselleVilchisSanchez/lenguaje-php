445dfe6fa14f9c872394f8dfff189188db527fc9
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-21 20:36:37 -0600

    Add files via upload

---
+++ ejercicio2.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+    <?php
+    $edad= 20;
+    $peso= 90.25;
+    $nombre= "Juan";
+    $existe= true;
+    echo "Variable integer";
+    echo $edad;
+    echo "<br>";
+    echo "Variable double";
+    echo $peso;
+    echo "<br>";
+    echo "Variable string:";
+    echo $nombre;
+    echo "<br>";
+    echo "Variable boolean:";
+    echo $existe;
+## Este es un comentario
+    ?>
+</body>
+</html>

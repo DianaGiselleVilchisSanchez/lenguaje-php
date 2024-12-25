7642084d001c7ce72cb9130549debfc2b6893f1d
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-24 22:50:36 -0600

    Add files via upload

---
+++ ejercicio7.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Problema</title>
+    </head>
+    
+    <body>
+        <?php
+        ## Mostramos numeros de los dias del 1 a la fecha actual
+        $dia =date("d");
+        $inicio = 1;
+        while ($inicio <= $dia){
+            echo $inicio."<br>";
+            $inicio++;
+        }
+        ?>
+</body>
+</html>

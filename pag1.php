29ebc1803446d4c1c9171904421e426f8a454318
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-25 22:30:26 -0600

    Add files via upload

---
+++ pag1.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Formulario de entrada de datos</title>
+    </head>
+    
+    <body>
+        <form method= "post" action= pag2.php>
+            Ingrese su nombre:
+        <input type= "text" name = "nombre">
+        <br>
+        <input type = "submit" value = "confirmar">
+</form>
+</body>
+</html>

70ddf1b4d60128a6662e91f5f4e6767e893496a8
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-26 19:23:32 -0600

    Add files via upload

---
+++ pagi1.php
@@ -0,0 +1,11 @@
+
+  <title>Cálculo de IMC</title>
+</head>
+<body>
+  <form action="pagi2.php" method="post">
+    Ingrese su peso (en kg): <input type="float" name="peso"><br>
+    Ingrese su estatura (en metros): <input type="float" name="estatura"><br>
+    <input type="submit" name="calcular" value="Calcular IMC">
+  </form>
+</body>
+</html>

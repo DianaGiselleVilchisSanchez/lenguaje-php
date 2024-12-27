61e5b481b1b561511a34baaebf91f833386bf98b
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-26 19:19:24 -0600

    Add files via upload

---
+++ pag2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Captura de datod del form</title>
+    </head>
+    
+    <body>
+        <?php
+        echo "El nombre ingresado es: ";
+        echo $_REQUEST['nombre'];
+        ?>
+</body>
+</html>

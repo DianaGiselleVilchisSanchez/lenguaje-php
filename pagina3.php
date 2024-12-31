9dcfe111f69146e2b17219b9a751b1d6e3bffda7
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-31 13:12:29 -0600

    Add files via upload

---
+++ pagina3.php
@@ -0,0 +1,13 @@
+<html> 
+<head> 
+<title>Problema</title> 
+</head> 
+<body> 
+<form action="pagina4.php" method="post"> 
+Ingrese nombre del curso: 
+<br> 
+<input type="text" name="nombrecurso" size="40"> 
+<input type="submit" value="Confirmar"> 
+</form> 
+</body> 
+</html> 
